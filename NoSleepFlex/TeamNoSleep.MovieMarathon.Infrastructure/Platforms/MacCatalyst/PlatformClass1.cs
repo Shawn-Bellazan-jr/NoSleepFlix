38f807e688df156ecfc69cce8332c0ece7cf94df
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-05 01:44:10 -0500

    feat(core): Implement clean architecture structure
    Implements a clean architecture structure for the application, following the SOLID principles and best practices.
    Adds a Core layer for business logic, Infrastructure layer for external dependencies, and Presentation layer for UI components.
    Introduces use cases, entities, and interfaces for a robust and testable application.

---
+++ NoSleepFlex/TeamNoSleep.MovieMarathon.Infrastructure/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamNoSleep.MovieMarathon.Infrastructure
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ NoSleepFlex/TeamNoSleep.MovieMarathon.Infrastructure/Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamNoSleep.MovieMarathon.Infrastructure
+{
+    // All the code in this file is only included on Windows.
+    public class PlatformClass1
+    {
+    }
+}
